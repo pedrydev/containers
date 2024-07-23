e610c7f513bfa129ace8b6619d24d975d6e9fdcc
Pedro Velazquez Borrero <pedrydev@outlook.com>
2024-07-23 09:30:30 -0400

    default vars

--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "mongodb-port" {
   description = "MongoDB port"
   type        = number
+  default     = 27017
 }
 
 variable "redis-port" {
   description = "Redis port"
   type        = number
+  default     = 6379
 }
 
 variable "rabbitmq-port" {
   description = "Rabbitmq port"
   type        = number
+  default     = 5672
 }
 
 variable "rabbitmq-management-port" {
   description = "Rabbitmq management port"
   type        = number
+  default     = 15672
 }
 
 variable "minio-port" {
   description = "MinIO port"
   type        = number
+  default     = 9001
 }
